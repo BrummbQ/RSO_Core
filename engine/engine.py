e157373604a8d13ef25edd1e0ef230c6c9879177
Gregor Tätzner <gregor@freenet.de>
2013-06-13 11:29:46 +0200

    Setup repository and commit initial structure and data types

---
+++ engine/engine.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+'''
+Copyright 2013  Gregor Tätzner gregor@freenet.de
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of
+the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+
+class Engine:
+
+    def __init__(self, customer_catalog, article_catalog, tag_catalog):
+        self.customer_catalog = customer_catalog
+        self.article_catalog = article_catalog
+        self.tag_catalog = tag_catalog
+
+    def articlelist_customer(customer):
+        pass
---
+++ types/article.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+'''
+Copyright 2013  Gregor Tätzner gregor@freenet.de
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of
+the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+
+class ArticlePrice:
+
+    def __init__(self, regular, action):
+        self.regular_price = regular
+        self.action_price = action
+
+
+class Limit:
+
+    def __init__(self, time, number):
+        self.time = time
+        self.number = number
+
+
+class Article:
+
+    def __init__(self, name, tags, price, limit):
+        self.name = name
+        self.tags = tags
+        self.price = price
+        self.limit = limit
